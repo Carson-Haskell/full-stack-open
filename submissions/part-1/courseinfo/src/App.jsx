39e7b5017ca7bbc2d2abe96f0ece14b7223bb8b0
Carson Haskell <carson.haskell@gmail.com>
2023-06-16 15:49:08 -0700

    complete exercise 1.4

--- submissions/part-1/courseinfo/src/App.jsx
+++ submissions/part-1/courseinfo/src/App.jsx
@@ -5,28 +5,28 @@ import Total from "./Total";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundementals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundementals of React",
+      exercises: 10,
+    },
+
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total
-        exercise1={part1.name}
-        exercise2={part2.name}
-        exercise3={part3.name}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
