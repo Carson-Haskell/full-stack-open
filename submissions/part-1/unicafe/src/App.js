70b83ab8dee73d682deb7be25015351dd34d4b91
Carson Haskell <carson.haskell@gmail.com>
2023-06-19 13:38:30 -0700

    complete exercise 1.10

--- submissions/part-1/unicafe/src/App.js
+++ submissions/part-1/unicafe/src/App.js
@@ -8,10 +8,6 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const total = good + neutral + bad;
-  const average = total / 3;
-  const positiveFeedback = (good / (total - neutral)) * 100;
-
   return (
     <div>
       Give Feedback
@@ -19,14 +15,7 @@ function App() {
       <Button handleClick={() => setGood(good + 1)} text="Good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        total={total}
-        average={average}
-        positiveFeedback={positiveFeedback}
-      />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
--- submissions/part-1/unicafe/src/Statistics.jsx
+++ submissions/part-1/unicafe/src/Statistics.jsx
@@ -1,26 +1,28 @@
-const Statistics = ({
-  good,
-  neutral,
-  bad,
-  total,
-  average,
-  positiveFeedback,
-}) => {
+import StatisticLine from "./StatisticLine";
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = total / 3;
+  const positiveRatings = (good / (total - neutral)) * 100;
+
   if (total === 0) return <p>No feedback given</p>;
+
   return (
     <>
       <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total Ratings: {total}</p>
-      <p>Average Rating: {average.toFixed(2)}</p>
-      <p>
-        Positive Ratings:{" "}
-        {positiveFeedback > 0
-          ? `${positiveFeedback.toFixed(2)}%`
-          : "No positive reviews"}
-      </p>
+      <StatisticLine text="Good" value={good} />
+      <StatisticLine text="Neutral" value={neutral} />
+      <StatisticLine text="Bad" value={bad} />
+      <StatisticLine text="Total Ratings" value={total} />
+      <StatisticLine text="Average Rating" value={average.toFixed(2)} />
+      <StatisticLine
+        text="Positive Ratings"
+        value={
+          positiveRatings > 0
+            ? `${positiveRatings.toFixed(2)}%`
+            : "No positive reviews"
+        }
+      />
     </>
   );
 };
