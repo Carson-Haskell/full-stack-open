76c536b20eb10add4c003aab88b8a4915e8f533a
Carson Haskell <carson.haskell@gmail.com>
2023-06-20 15:08:56 -0700

    complete exercise 2.9

--- submissions/part-2/phonebook/src/App.js
+++ submissions/part-2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const addPerson = event => {
     event.preventDefault();
@@ -32,9 +38,28 @@ const App = () => {
 
   const checkDuplicate = name => persons.find(person => person.name === name);
 
+  const filterByName = () => {
+    const filtered = persons.filter(person => {
+      const name = person.name.toLowerCase();
+      return name.includes(filter);
+    });
+
+    return filtered;
+  };
+
+  const filteredPeople = filterByName();
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search by name:
+        <input
+          value={filter}
+          onChange={event => setFilter(event.target.value)}
+        />
+      </div>
+      <h2>Add a new person</h2>
       <form onSubmit={addPerson}>
         <div>
           Name:{" "}
@@ -55,15 +80,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name, number }) => (
+      {filteredPeople.map(({ name, number }) => (
         <p key={name}>
           {name} {number}
         </p>
       ))}
       <br />
-      {/* <div>Debug: {newNumber} </div>
-      <div>Debug: {newName}</div>
-      <div>Debug: {JSON.stringify(persons)}</div> */}
     </div>
   );
 };
